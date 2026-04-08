8344a77495ff5467f2bbd16830fbd32c45a35f0e
dictswa <127397085+dictswa@users.noreply.github.com>
2026-04-08 11:27:58 +0200

    Update Project_1.py
    scatterplot

--- Project_1.py
+++ Project_1.py
@@ -6,6 +6,13 @@ Created on Wed Apr  8 10:58:33 2026
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv("jain.txt", sep="\s+", header=None)
 
+x = df[0]
+y = df[1]
+
+plt.scatter(x,y)
+plt.show()
